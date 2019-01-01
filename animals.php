2282974c19917e2a48dbdf2894fd0493fb3e7647
Юлия Бордачева <s_proza@list.ru>
2019-01-01 17:53:38 +0300

    3comm

--- animals.php
+++ animals.php
@@ -2,7 +2,7 @@
   <html lang="en">
     <head>
       <meta charset="UTF-8">
-      <title>Шаблон</title>
+      <title>Животные</title>
     </head>
     <body>
       <h1>Животные</h1>
@@ -23,20 +23,54 @@ $animals = ['Europe' => ['Cervus elaphus', 'Erinaceus europaeus'],
 	             'Southern America' => ['Cacajao calvus', 'Vicugna pacos', 'Lagidium']];
 
 
-$countinent = ['Europe','Asia','Africa','Australia','India','Antarctida','North America','Southern America'];
+
  
 $animals2 = []; 
 foreach($animals as $areaName => $animalArray) {
     foreach ($animalArray as $animalName) {
         if (strpos($animalName, ' ') !== false) {
+          $animals2[] = $animalName;
         }
     }
 }
-print_r($animals2);
-list($a,$b) = array_slice($animals2, 0, 1);
-shuffle($a); 
- shuffle($b);
- $a1 = implode (",", $a);
- $b1 = implode (",", $b);
+print_r($animals2); 
+
+$b = implode(' ', $animals2);
+
+var_dump($b); 
  
+ $c = explode(' ', $b);
+
+ var_dump($c);
+
+ 
+
+ foreach ($c as $key => $value) {
+        if (($key + 1) % 2 != 0) {
+            $even[] = $value;
+            print_r($even) . '<br />';
+            echo '<pre>';
+        }
+    }
+
+ foreach ($c as $key => $value) {
+        if (($key + 1) % 2 == 0) {
+            $odd[] = $value;
+            print_r($odd) . '<br />';
+            echo '<pre>';
+        }
+    }
+
+    shuffle($even);
+    print_r($even);
+    
+$result1 = array_combine($even, $odd);
+print_r($result1);
+
+foreach ($result1 as $key => $value) {
+  $result2 =  $key  . ' ' .  $value . '<br>';
+  print_r($result2);
+}
+
+
 ?>
